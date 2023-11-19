d910050b4bd23abeac6f6252b6ce44f8d1b41bfc
ericnkaka <nkakaeric96@gmail.com>
2023-11-19 12:56:15 +0200

    feat: done

--- helpers.mjs
+++
@@ -1,8 +0,0 @@
-export const connectToDatabase = () => {
-    const dummyPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Resolved")
-        }, 1000)
-    })
-    return dummyPromise
-}
